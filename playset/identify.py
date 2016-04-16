54347219123c2f4ebe227a08d4f469b801744e31
SUE\Elmacho <chao@elmagincapital.com>
2016-04-16 15:18:57 -0600

    find 12 cards in image

--- playset/identify.py
+++ playset/identify.py
@@ -5,7 +5,7 @@ import sys
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from card_img import getCards
+from card_img import getCards, rectify
 
 # a = cv2.goodFeaturesToTrack(image=(1),maxCorners=5,qualityLevel=0.1,minDistance=100)
 #
@@ -150,6 +150,30 @@ def guess_shape(features):
         return Diamond
     else:
         return Squiggle
+
+
+
+
+def extract_card(contour, im):
+    warp = None
+    peri = cv2.arcLength(contour, True)
+    h = cv2.approxPolyDP(contour, 0.02 * peri, True)
+    if h.shape[0] > 4 :
+        return warp
+    approx = rectify(h)
+    h = np.array([[0, 0], [449, 0], [449, 449], [0, 449]], np.float32)
+    transform = cv2.getPerspectiveTransform(approx, h)
+    warp = cv2.warpPerspective(im, transform, (450, 450))
+    return warp
+
+
+def get_card_contours(im, numcards):
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (1, 1), 1000)
+    flag, thresh = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY)
+    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:(numcards)]
+    return contours
 
 
 if __name__ == "__main__":
--- playset/scratch.py
+++ playset/scratch.py
@@ -2,20 +2,58 @@ import os
 
 import cv2
 import numpy as np
+from card_img import getCards
 
+from playset.attributes import Card
 from playset.attributes import Purple, Green, Red
-from playset.identify import CardRepo, get_features, id_attributes
+from playset.identify import CardRepo, get_features, id_attributes, get_card_contours, extract_card, center
 
-root = 'c:/users/elmacho/pycharmprojects/playset/data/training'
-repo = CardRepo(root)
+root = 'C:/Users/Elmacho/Downloads/documents-export-2016-04-11'
+filename = '20160411_210523.jpg'
+orig = cv2.imread(os.path.join(root, filename))
+img = cv2.resize(orig,(0,0),None,fx=0.5,fy=0.5)
+card_contours = [x for x in get_card_contours(img, 16)]
+cp = img.copy()
+# cv2.drawContours(cp, card_contours, -1, (0, 255, 0), 3)
 
-card = repo.get(1)
-cv2.imshow('image',card)
-attributes = id_attributes(card.copy())
-print(attributes.color)
-print(attributes.count)
-print(attributes.shading)
-print(attributes.shape)
+n = len(card_contours)
+attr = [None] * n
+icp = img.copy()
+for i in range(0, n):
+    contour = card_contours[i]
+    card = extract_card(contour, img)
+    if card is None:
+        continue
+    try:
+        a = id_attributes(card) # type: Card
+        attr[i] = a
+        x,y,w,h = cv2.boundingRect(contour)
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        size = 0.75
+        inc = 25
+        x0 = x+50
+        y0 = y+50
+        cv2.putText(icp,str(a.count),(x0,y0), font, size,(0,0,0),2,cv2.LINE_AA)
+        cv2.putText(icp,str(a.color.name),(x0,y0+inc), font, size,(0,0,0),2,cv2.LINE_AA)
+        cv2.putText(icp,str(a.shading),(x0,y0+inc*2), font, size,(0,0,0),2,cv2.LINE_AA)
+        cv2.putText(icp,str(a.shape),(x0,y0+inc*3), font, size,(0,0,0),2,cv2.LINE_AA)
+        cv2.drawContours(icp,card_contours,i,(0,255,0),3)
+    except:
+        pass
+cv2.imshow('image',icp)
+
+
+# root = 'c:/users/elmacho/pycharmprojects/playset/data/training'
+# repo = CardRepo(root)
+#
+# card = repo.get(1)
+# cv2.imshow('image',card)
+# attributes = id_attributes(card.copy())
+# print(attributes.color)
+# print(attributes.count)
+# print(attributes.shading)
+# print(attributes.shape)
 
 
 
